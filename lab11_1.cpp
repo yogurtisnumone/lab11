b4e231c8509b251c8451c3c553735b63235317a0
kachapat_p <kachapat_p@cmu.ac.th>
2024-01-02 11:31:03 +0700

    1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,45 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int x = rand()%9;
+    string grade;
+    switch (x)
+    {
+    case 0:
+        grade = "A";
+        break;
+    case 1:
+        grade = "B+";
+        break;
+    case 2:
+        grade = "B";
+        break;
+    case 3:
+        grade = "C+";
+        break;
+    case 4:
+        grade = "C";
+        break;
+    case 5:
+        grade = "D+";
+        break;
+    case 6:
+        grade = "D";
+        break;
+    case 7:
+        grade = "F";
+        break;
+    default:
+        grade = "W";
+        break;
+    }
+    cout << "You will get " << grade << " in this 261102." ;
+}
+
